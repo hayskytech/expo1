b088652ab684659ed621466e7ef8e21e0dfd1e43
Sufyan Shaik <mdsufyan7@gmail.com>
2024-02-14 10:13:36 +0530

    project setup

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={{ fontSize: 30 }}>Haysky</Text>
+      <Text>Hello world. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consectetur et animi magnam consequuntur cupiditate deleniti velit dignissimos? Similique asperiores voluptatem velit eligendi dolorem, quidem quos tempore nesciunt, minus expedita reiciendis.</Text>
+      <Button title='Hello' />
+
       <StatusBar style="auto" />
     </View>
   );
